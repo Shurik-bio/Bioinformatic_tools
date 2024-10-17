9eb30df4ca0330aa7831a04c4bed47aede87e68e
Aleksandra <amikhay4lova@gmail.com>
2024-10-17 16:28:12 +0300

    Added path to files

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -1,4 +1,5 @@
 import os
+
 path_fasta = "/home/shurik/course_materials/Homeworks/HW5_Files/example_data"
 file_fasta_name = "example_multiline_fasta.fasta"
 fullparth_fasta = os.path.join(path_fasta, file_name)
@@ -6,6 +7,7 @@ fullparth_fasta = os.path.join(path_fasta, file_name)
 path_blast = "/home/shurik/course_materials/Homeworks/HW5_Files/example_data"
 file_blast_name = "example_blast_results.txt"
 fullparth_blast = os.path.join(path_blast, file_blast_name)
+
 
 def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str) -> str:
     """
@@ -20,12 +22,12 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str) -> s
     """
 
     with open(fullparth, "r") as input_fasta, open("output.fasta", "w") as output_fasta:
-        sequence = ''
+        sequence = ""
         for line in input_fasta:
             line = line.strip()
             if line.startswith(">"):
                 output_file.write(line + "\n")
-                sequence = ''
+                sequence = ""
             else:
                 sequence += line
         output_fasta.write(sequence)
@@ -43,7 +45,9 @@ def parse_blast_output(input_file: str, output_file: str) -> str:
         A path to the output file
     """
 
-    with open(fullparth_blast, "r") as input_file, open("output.txt", "w") as output_file:
+    with open(fullparth_blast, "r") as input_file, open(
+        "output.txt", "w"
+    ) as output_file:
         lines = input_file.readlines()
         protein_describtion = []
 
